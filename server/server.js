a9fa8f295f057fb83e238f4c1656dd62ca58624d
Daniel <lehoczki.dani5@gmail.com>
2026-08-03 22:04:30 +0200

    * changed to /*splat

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use("/api/user", userRoutes)
 
 if (isProduction) {
     app.use(express.static(path.join(__dirname, "../client/dist")))
-    app.get("*", (req, res) => {
+    app.get("/*splat", (req, res) => {
         res.sendFile(path.join(__dirname, "../client/dist/index.html"))
     })
 }
